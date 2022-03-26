4b3756a519ca11f993334408310d18301e7f18cf
moezguer <ozgr.mustafa@gmail.com>
2022-03-26 01:44:41 +0100

    Kubernetes integrated, skaffold config added.

--- posts/index.js
+++ posts/index.js
@@ -9,11 +9,11 @@ app.use(bodyParser.json())
 app.use(cors())
 const posts = {}
 
-app.get('/posts', (req, res) => {
-  res.send(posts)
-})
+// app.get('/posts', (req, res) => {
+//   res.send(posts)
+// })
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const postId = randomBytes(4).toString('hex')
 
   const { title } = req.body
@@ -22,7 +22,7 @@ app.post('/posts', async (req, res) => {
     id: postId, title
   }
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id: postId, title
@@ -41,5 +41,6 @@ app.post('/events', ((req, res) => {
 }))
 
 app.listen(4000, () => {
+  console.log('v30')
   console.log('Listening on 4000')
 })
